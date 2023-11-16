b8c9e4d75a611f9220e6b366fa0779237333e19c
shenyuming <m18515607030@163.com>
2023-11-16 10:01:10 +0800

    访问

--- 练习/text_one.py
+++ 练习/text_one.py
@@ -8,7 +8,9 @@
 '''
 from selenium import webdriver
 import time
+from selenium.webdriver.common.by import By
 driver = webdriver.Chrome()
+driver.find_element(By.CSS_SELECTOR,"XX").clear()
 
 print('selenium特性')
 if __name__ == '__main__':
